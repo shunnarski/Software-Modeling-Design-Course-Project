265772c87956f3890c3e37516a1fa49622bd3c39
alecshunnarah <alecshunnarah@172.17.142.210>
2017-10-18 21:46:52 -0500

    UI Update

--- src/SMDUI/HomeUI.java
+++ src/SMDUI/HomeUI.java
@@ -86,7 +86,8 @@ public class HomeUI extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
+        this.setVisible(false);
+        new CheckoutUI().setVisible(true);
     }//GEN-LAST:event_jButton2ActionPerformed
 
     // if "Manage Products" is clicked
